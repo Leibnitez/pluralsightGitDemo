0b160c532a491956e5ac68bcbef442f9eeb50413
Leibnitez <contact.issam@gmail.com>
2021-03-21 17:54:55 +0100

    Add project

---
+++ PocPluralsightGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PocPluralsightGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello first poc!");
+        }
+    }
+}
